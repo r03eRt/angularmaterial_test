40ec08d2fc4e00c69b728233d9538356515fe8fe
r03eRt <morgadoluengo@gmail.com>
2016-09-12 16:01:46 +0200

    added responsive navbar +bottomSheet

--- assets/js/UserController.js
+++ assets/js/UserController.js
@@ -1,9 +1,32 @@
-app.controller('UserController',['userService', ' $mdBottomSheet',function(userService, $mdBottomSheet){
+app.controller('UserController',['userService','$mdBottomSheet','$mdSidenav',function(userService, $mdBottomSheet, $mdSidenav){
     var self = this;
 
     this.selected     = null;
     this.users        = [ ];
+
+
+    this.selectUser = function (user) {
+        this.selected = user;
+    };
+
+
+    this.shareContact = function (selectedUser) {
+        //console.log(selectedUser);
+       $mdBottomSheet.show({
+           controllerAs: "vm",
+           controller: "ContactSheetController",
+           templateUrl: 'assets/view/contactSheet.html',
+           locals: {
+               Item: selectedUser
+           }
+       });
+    };
     
+    this.toggleList = function () {
+        $mdSidenav('left').toggle();
+    }
+
+
     userService
         .loadAllUsers()
         .then( function( users ) {
@@ -11,19 +34,25 @@ app.controller('UserController',['userService', ' $mdBottomSheet',function(userS
             self.selected = users[0];
         });
 
-    this.selectUser = function (user) {
-        this.selected = user;
-    };
-    
-    this.makeContact = function (selectedUser) {
-        $mdBottomSheet.show(
-            {
-                controllerAs     : "vm",
-                controller       : [ '$mdBottomSheet', ContactSheetController],
-                templateUrl      : './src/users/view/contactSheet.html',
-                parent           : angular.element(document.getElementById('content'))
-            }
-        );
-    };
+
+}]);
+
+
+app.controller('ContactSheetController',['$mdBottomSheet','Item',function ($mdBottomSheet,Item) {
+    console.log(Item);
+    this.user = Item;
+    console.log(this.user);
+    this.items = [
+        { name: 'Phone'       , icon: 'phone'       , icon_url: 'assets/img/phone.svg'},
+        { name: 'Twitter'     , icon: 'twitter'     , icon_url: 'assets/img/twitter.svg'},
+        { name: 'Google+'     , icon: 'google_plus' , icon_url: 'assets/img/google_plus.svg'},
+        { name: 'Hangout'     , icon: 'hangouts'    , icon_url: 'assets/img/hangouts.svg'}
+    ];
+
+    this.closeBottomSheet = function () {
+        console.log('close');
+        $mdBottomSheet.hide();
+    }
+
 
 }]);
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.config(function( $mdIconProvider ){
 
-  // Register the user `avatar` icons
-  $mdIconProvider.defaultIconSet("assets/img/avatars.svg", 128)
+  $mdIconProvider.defaultIconSet("assets/img/avatars.svg", 128);
+  $mdIconProvider.icon("share", "assets/img/share.svg", 24);
+  $mdIconProvider.icon("close", "assets/img/close.svg", 24);
+  $mdIconProvider.icon("menu", "assets/img/menu.svg", 24);
 });
